4bc611e1fa9508d362b2e13f83da6933ed8481c9
hessj1994 <hessj1994@gmail.com>
2020-04-03 14:48:00 -0400

    First Commit

---
+++ 01_KomodoCafe/ProgramProperties.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_KomodoCafe
+{
+    class ProgramProperties
+    {
+        public TYPE Type { get; set; }
+        public TYPE Type { get; set; }
+        public TYPE Type { get; set; }
+        public TYPE Type { get; set; }
+        public TYPE Type { get; set; }
+        public TYPE Type { get; set; }
+    }
+}
